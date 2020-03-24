50b4aae3be45b71a53e8025a5e5dc4bb44735507
Simon Kilroy <simonkilroy1@gmail.com>
2020-03-24 11:38:45 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -5,20 +5,20 @@ function basicTeenager(age) {
 }
 
 function teenager(age) {
-  if (age >= 13 && <= 19) {
-    return `You are a teenager!`
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager!";
 } else {
-  return `You are not a teenager`
+  return "You are not a teenager";
 }
 
 function ageChecker(age) {
-  if (age >= 13 && <= 19) {
-    return `You are a teenager!`
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager!"
 } else if {
   if (age <= 12) {
-    return `You are a kid`
+    return "You are a kid"
   } else {
-    return `You are a grownup`
+    return "You are a grownup"
   }
 }
 
