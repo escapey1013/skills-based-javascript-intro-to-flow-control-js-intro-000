f25fe875bc16bbbdf5ce676d2b5c0f33e660903e
Simon Kilroy <simonkilroy1@gmail.com>
2020-03-24 11:48:11 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -14,13 +14,11 @@ function teenager(age) {
 
 function ageChecker(age) {
   if (age >= 13 && age <= 19) {
-    return "You are a teenager!"
-} else if {
-  if (age <= 12) {
-    return "You are a kid"
-}
+    return "You are a teenager!";
+} else if (age <= 12) {
+    return "You are a kid";
 } else {
-    return "You are a grownup"
+    return "You are a grownup";
 }
 }
 
